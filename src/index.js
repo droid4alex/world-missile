3703969a03f3c9259d5e495e2fca4724c1239c19
droid4alex <droid4alex@gmail.com>
2021-03-08 08:47:42 -0800

    Adds missile, explosion, game logic

--- src/index.js
+++ src/index.js
@@ -1,55 +1,124 @@
-import Unit from './unit'
+import Explosion from './explosion';
 import Missile from './missile'
 
+const COUNTRIES = [{ country: 'Canada', x: 0.13, y: 0.23 }, { country: 'United States', x: 0.15, y: 0.38 }, { country: 'Mexico', x: 0.15, y: 0.50 }, { country: 'Brazil', x: 0.30, y: 0.73 }, { country: 'European Union', x: 0.53, y: 0.32 }, { country: 'Africa', x: 0.56, y: 0.56 }, { country: 'Russia', x: 0.80, y: 0.24 }, { country: 'China', x: 0.85, y: 0.45 }, { country: 'India', x: 0.75, y: 0.48 }, { country: 'Australia', x: 0.96, y: 0.84 }]
+const COLORS = ["Crimson ", "Green", "Red", "Orange", "LimeGreen", "Salmon", "Tomato", "Lime", "OrangeRed", "Coral"]
 const canvas = document.getElementsByTagName("canvas")[0];
 const c = canvas.getContext('2d');
 const level = document.getElementById("level-count");
 const intercepted = document.getElementById("intercepted-count");
 const score = document.getElementById("score-count");
-var levelCount = 0;
-var interceptedCount = 0;
-var scoreCount = 0;
+
+let timeStart = new Date()
+let timeStop = new Date();
+let levelCount = 0;
+let interceptedCount = 0;
+let scoreCount = 0;
+let missiles = [];
+let explosions = [];
 
 canvas.width = window.innerWidth * 0.95;
 canvas.height = window.innerHeight * 0.90;
+let img = new Image;
+img.onload = function () {
+  c.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
 
-c.fillStyle = "gray";
-c.fillRect(0, 0, canvas.width, canvas.height);
+img.width = canvas.width;
+img.height = canvas.height;
+img.src = 'world-map.png';
 
-// const unitImage = new Image();
-// unitImage.src = './src/unit.png'
-// c.drawImage(unitImage, 50, 50);
+function generateMissile(color){
+  let random = Math.floor(Math.random() * 10);
+  let xSpeed = Math.random() * 2;
+  let ySpeed = 2 - xSpeed;
+  if ((Math.random() * 2) >= 1) {
+    xSpeed = xSpeed * -1;
+  }
+  if ((Math.random() * 2) >= 1) {
+    ySpeed = ySpeed * -1;
+  }
+  missiles.push(
+    new Missile(COUNTRIES[random].country, canvas.width * COUNTRIES[random].x, canvas.height * COUNTRIES[random].y, 10, 10, color, { x: xSpeed, y: ySpeed}, canvas, c)
+  )
+}
 
-const unit1 = new Unit(25, 25, 25, "brown", canvas, c);
-const unit2 = new Unit(25, canvas.height - 25, 25, "brown", canvas, c);
-const unit3 = new Unit(canvas.width - 25, 25, 25, "brown", canvas, c);
-const unit4 = new Unit(canvas.width - 25, canvas.height - 25, 25, "brown", canvas, c);
-unit1.render();
-unit2.render();
-unit3.render();
-unit4.render();
-const missile1 = new Missile(25, 25, 100, 2, "red", 0, canvas, c);
-const missile2 = new Missile(25, canvas.height - 25, 100, 2, "red", 0, canvas, c);
-const missile3 = new Missile(canvas.width - 125, 25, 100, 2, "red", 0, canvas, c);
-const missile4 = new Missile(canvas.width - 125, canvas.height - 25, 100, 2, "red", 0, canvas, c);
-missile1.render();
-missile2.render();
-missile3.render();
-missile4.render();
+function animate(){
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.drawImage(img, 0, 0, canvas.width, canvas.height);
+  missiles.forEach(missile => {
+    missile.render()
+    missile.fly()
+  })
+  explosions.forEach(explosion => {
+    explosion.render()
+  })
+  let timeStop = new Date();
+  if (Math.floor(Math.abs((timeStart.getTime() - timeStop.getTime()) / 1000)>59)){
+    alert("You lost! All missiles must be intercepted within 60 seconds.")
+    levelCount = levelCount - 1;
+    startLevel()
+  }
+}
 
 canvas.addEventListener('click', (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
-  interceptedCount = interceptedCount + 1/4;
-  intercepted.innerHTML = "Missiles Intercepted: " + interceptedCount;
-  scoreCount = scoreCount + 1;
-  score.innerHTML = "Score: " + scoreCount;
-  levelCount = Math.floor(scoreCount / 10);
-  level.innerHTML = "Level: " + levelCount;
+  let missilesRemaining = [];
+  let mouseX = e.pageX - canvas.offsetLeft;
+  let mouseY = e.pageY - canvas.offsetTop;
+  missiles.forEach(missile => {
+    if (Math.abs(mouseX - missile.x) <= 50 && Math.abs(mouseY - missile.y) <= 50) {
+      explosions.push(new Explosion(missile, canvas, c))
+      interceptedCount = interceptedCount + 1;
+      intercepted.innerHTML = "Missiles Intercepted: " + interceptedCount + " of " + levelCount*5;
+      console.log(missile)
+    }
+    else {
+      missilesRemaining.push(missile)
+    }
+  })
+  missiles = missilesRemaining;  
 })
 
-// canvas.addEventListener('mousemove', () => {
-//   const cRect = canvas.getBoundingClientRect(); 
-//   console.log(cRect)
-// })
+canvas.addEventListener('mousemove', (e) => {
+  let mouseX = e.pageX - canvas.offsetLeft;
+  let mouseY = e.pageY - canvas.offsetTop;
+  if (missiles.length > 0){
+    missiles.forEach(missile => {
+      if (Math.abs(mouseX - missile.x) <= 50 && Math.abs(mouseY - missile.y) <= 50){
+        missile.targetOn()
+      } else{
+        missile.targetOff()
+      }
+    })
+  }
+  if (interceptedCount === levelCount * 5){
+    let timeStop = new Date();
+    let seconds = Math.abs((timeStart.getTime() - timeStop.getTime())/1000);
+    scoreCount = scoreCount + 100;
+    if (Math.floor(seconds)<15){
+      scoreCount = scoreCount + (1500 - Math.floor(seconds)*100);
+    }
+    score.innerHTML = "Score: " + scoreCount;
+    alert("Great job! You beat level " + levelCount + " in " + seconds.toFixed(2) + " seconds!")
+    startLevel()
+  }
+})
 
+function startLevel(){
+  interceptedCount = 0;
+  intercepted.innerHTML = "Missiles Intercepted: " + interceptedCount;
+  missiles = [];
+  explosions = [];
+  timeStart = new Date();
+  levelCount = levelCount + 1;
+  level.innerHTML = "Level: " + levelCount;
+  let random = Math.floor(Math.random() * 10);
+  let color = COLORS[random];
+  for (let i = 0; i < (5*levelCount); i++){
+    generateMissile(color);    
+  }
+}
+
+startLevel();
+animate();
--- src/missile.js
+++ src/missile.js
@@ -1,22 +1,56 @@
+
 export default class Missile {
-  constructor(x, y, width, height, color, speed, canvas, c) {
+  constructor(country, x, y, width, height, color, speed, canvas, c) {
+    this.country = country
     this.x = x
     this.y = y
-    this.width = width
-    this.height = height
+    this.width = width * speed.x
+    this.height = height * speed.y
     this.color = color  
     this.speed = speed
     this.canvas = canvas
     this.c = c
-
-    console.log("Missile instance constructed")
+    this.targeted = false
   }
+
 
   render() {
     this.c.beginPath()
     this.c.rect(this.x, this.y, this.width, this.height)
+    // this.c.arc(this.x, this.y, this.width, 7, 7 * Math.PI, false);
     this.c.fillStyle = this.color
     this.c.fill()
+    if (this.targeted) {
+      this.c.beginPath();
+      this.c.arc(this.x, this.y, 50, 5, 5 * Math.PI);
+      this.c.strokeStyle = "gray";
+      this.c.stroke();
+    }
+  }
+
+  fly(){
+    this.x = this.x + this.speed.x
+    this.y = this.y + this.speed.y
+    if (this.x < 0){
+      this.x = this.canvas.width
+    }
+    if (this.x > this.canvas.width) {
+      this.x = 0
+    }
+    if (this.y < 0) {
+      this.y = this.canvas.height
+    }
+    if (this.y > this.canvas.height) {
+      this.y = 0
+    }
+  }
+
+  targetOn(){
+    this.targeted = true;
+  }
+
+  targetOff() {
+    this.targeted = false;
   }
 }
 
